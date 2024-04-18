961e62881cc5dde56590606821274f007a23ee09
Ting2104 <tchen@edu.tecnocampus.cat>
2024-04-18 21:15:26 +0200

    Mejora cámara
    Simplificar, eliminar y modificar el código.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,36 +4,37 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform followTransform;
-    public BoxCollider2D mapBounds;
+    public GameObject playerFollow;
 
     public float xMin, xMax, yMin, yMax;
     private float camY, camX;
-    private float camOrthsize;
-    private float cameraRatio;
-    private Camera mainCam;
-    private Vector3 smoothPos;
-    public float smoothSpeed = 0.5f;
+    private float playerX, playerY;
+    public float speed = 0.5f;
 
     private void Start()
     {
-        xMin = mapBounds.bounds.min.x;
-        xMax = mapBounds.bounds.max.x;
-        yMin = mapBounds.bounds.min.y;
-        yMax = mapBounds.bounds.max.y;
-        mainCam = GetComponent<Camera>();
-        camOrthsize = mainCam.orthographicSize;
-        cameraRatio = (xMax + camOrthsize) / 2.0f;
+        camX = playerX + xMin;
+        camY = playerY + yMin;
+        transform.position = Vector3.Lerp(transform.position, new Vector3(camX, camY, -1), 1);
+    }
+    //Movimiento de la cámara 
+    void MoveCam()
+    {
+        if (playerFollow)
+        {
+            playerX = playerFollow.transform.position.x;
+            playerY = playerFollow.transform.position.y;
+            if (playerX > xMin && playerX < xMax)
+                camX = playerX;
+            if (playerY > yMin && playerY < yMax)
+                camY = playerY;
+        }
+        transform.position = Vector3.Lerp(transform.position, new Vector3(camX, camY, -1), speed * Time.deltaTime);
     }
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        camY = Mathf.Clamp(followTransform.position.y, yMin + camOrthsize, yMax - camOrthsize);
-        camX = Mathf.Clamp(followTransform.position.x, xMin + cameraRatio, xMax - cameraRatio);
-        smoothPos = Vector3.Lerp(this.transform.position, new Vector3(camX, camY, this.transform.position.z), smoothSpeed);
-        this.transform.position = smoothPos;
-
-
+        MoveCam();
     }
 
 }
